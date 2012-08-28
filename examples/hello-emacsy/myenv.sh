ac5f3e66b389f112f44ae9e06ba365390a4693b7
Shane Celis <shane@gnufoo.org>
2012-08-28 14:04:13 -0400

    Updated for the new module structure (emacsy emacsy).

--- examples/hello-emacsy/myenv.sh
+++ examples/hello-emacsy/myenv.sh
@@ -1,4 +1,4 @@
 # export GUILE_WARN_DEPRECATED=detailed
 # export GUILE_AUTO_COMPILE=0
 export DYLD_FALLBACK_LIBRARY_PATH=/opt/local/lib:/usr/local/lib:
-export GUILE_LOAD_PATH=/Users/shane/School/uvm/CSYS-395-evolutionary-robotics/bullet-2.79/Demos/GuileDemo/module:.
+export GUILE_LOAD_PATH=/Users/shane/School/uvm/CSYS-395-evolutionary-robotics/bullet-2.79/Demos/GuileDemo/module
