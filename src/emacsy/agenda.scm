6e00fe6af554e3be14f1a88ef2ec4de2ead35994
Shane Celis <shane@gnufoo.org>
2013-11-02 03:25:41 -0400

    Trying to switch over to guile-2d's coroutines and agenda. Having some trouble making codefine* work.

---
+++ src/emacsy/agenda.scm
@@ -0,0 +1,201 @@
+;;; guile-2d
+;;; Copyright (C) 2013 David Thompson <dthompson2@worcester.edu>
+;;;
+;;; Guile-2d is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU Lesser General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; Guile-2d is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Lesser General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with this program.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Deferred procedure scheduling.
+;;
+;;; Code:
+
+(define-module (emacsy agenda)
+  #:use-module (ice-9 q)
+  #:use-module (srfi srfi-1)
+  #:use-module (srfi srfi-9)
+  #:use-module (emacsy coroutine)
+  #:export (make-agenda
+            with-agenda
+            agenda-schedule
+            agenda-schedule-interval
+            update-agenda
+            clear-agenda
+            wait))
+
+;; This code is a modified version of the agenda implementation in
+;; SICP. Thank you, SICP!
+
+;;;
+;;; Time segment
+;;;
+
+(define-record-type <time-segment>
+  (%make-time-segment time queue)
+  time-segment?
+  (time segment-time)
+  (queue segment-queue))
+
+(define (make-time-segment time . callbacks)
+  "Create a new time segment at TIME and enqueus everything in the
+list CALLBACKS."
+  (let ((segment (%make-time-segment time (make-q))))
+    ;; Enqueue all callbacks
+    (for-each (lambda (c) (segment-enq segment c)) callbacks)
+    segment))
+
+(define (segment-enq segment callback)
+  "Add the CALLBACK procedure to SEGMENT's queue."
+  (enq! (segment-queue segment) callback))
+
+;;;
+;;; Agenda
+;;;
+
+(define-record-type <agenda>
+  (%make-agenda time segments)
+  agenda?
+  (time agenda-time set-agenda-time!)
+  (segments agenda-segments set-agenda-segments!))
+
+(define (make-agenda)
+  "Create a new, empty agenda."
+  (%make-agenda 0 '()))
+
+;; The global agenda that will be used when schedule is called outside
+;; of a with-agenda form.
+(define global-agenda (make-agenda))
+
+(define *current-agenda* global-agenda)
+
+;; emacs: (put 'with-agenda 'scheme-indent-function 1)
+(define-syntax-rule (with-agenda agenda body ...)
+  (begin
+    (set! *current-agenda* agenda)
+    body
+    ...
+    (set! *current-agenda* global-agenda)))
+
+(define (agenda-empty? agenda)
+  "Return #t if AGENDA has no scheduled procedures."
+  (null? (agenda-segments agenda)))
+
+(define (first-segment agenda)
+  "Return the first time segment in AGENDA."
+  (car (agenda-segments agenda)))
+
+(define (rest-segments agenda)
+  "Return everything but the first segment in AGENDA."
+  (cdr (agenda-segments agenda)))
+
+(define (agenda-add-segment agenda time callback)
+  "Add a new time segment to the beginning of AGENDA at the given TIME
+and enqueue CALLBACK."
+  (set-agenda-segments! agenda
+                        (cons (make-time-segment time callback)
+                              (agenda-segments agenda))))
+
+(define (insert-segment segments time callback)
+  "Insert a new time segment after the first segment in SEGMENTS."
+  (set-cdr! segments
+            (cons (make-time-segment time callback)
+                  (cdr segments))))
+
+(define (first-agenda-item agenda)
+  "Return the first time segment queue in AGENDA."
+  (if (agenda-empty? agenda)
+      (error "Agenda is empty")
+      (segment-queue (first-segment agenda))))
+
+(define (agenda-time-delay agenda dt)
+  "Return the sum of the time delta, DT, and the current time of AGENDA."
+  (+ (agenda-time agenda) (inexact->exact (round dt))))
+
+(define (%agenda-schedule agenda callback dt)
+  "Schedule the procedure CALLBACK in AGENDA to be run DT updates from now."
+  (let ((time (agenda-time-delay agenda dt)))
+    (define (belongs-before? segments)
+      (or (null? segments)
+          (< time (segment-time (car segments)))))
+
+    (define (add-to-segments segments)
+      ;; Add to existing time segment if the times match
+      (if (= (segment-time (car segments)) time)
+          (segment-enq (car segments) callback)
+          ;; Continue searching
+          (if (belongs-before? (cdr segments))
+              ;; Create new time segment and insert it where it belongs
+              (insert-segment segments time callback)
+              ;; Continue searching
+              (add-to-segments (cdr segments)))))
+
+    ;; Handle the case of inserting a new time segment at the
+    ;; beginning of the segment list.
+    (if (belongs-before? (agenda-segments agenda))
+        ;; Add segment if it belongs at the beginning of the list...
+        (agenda-add-segment agenda time callback)
+        ;; ... Otherwise, search for the right place
+        (add-to-segments (agenda-segments agenda)))))
+
+(define (flush-queue! q)
+  "Dequeue and execute every member of Q."
+  (unless (q-empty? q)
+    ((deq! q)) ;; Execute scheduled procedure
+    (flush-queue! q)))
+
+(define (%update-agenda agenda)
+  "Move AGENDA forward in time and run scheduled procedures."
+  (set-agenda-time! agenda (1+ (agenda-time agenda)))
+  (let next-segment ()
+    (unless (agenda-empty? agenda)
+      (let ((segment (first-segment agenda)))
+        ;; Process time segment if it is scheduled before or at the
+        ;; current agenda time.
+        (when (>= (agenda-time agenda) (segment-time segment))
+          (flush-queue! (segment-queue segment))
+          (set-agenda-segments! agenda (rest-segments agenda))
+          (next-segment))))))
+
+(define (%clear-agenda agenda)
+  "Remove all scheduled procedures from AGENDA."
+  (set-agenda-segments! agenda '()))
+
+(define* (agenda-schedule thunk #:optional (delay 1))
+  "Schedule THUNK in the current agenda to run after DELAY updates (1
+by default)."
+  (%agenda-schedule *current-agenda* thunk delay))
+
+(define* (agenda-schedule-interval thunk #:optional (interval 1) (delay 1))
+  "Schedule THUNK in the current agenda to run after DELAY updates and
+run every INTERVAL updates thereafter. Both DELAY and INTERVAL default
+to 1. Simply pass THUNK and nothing else to schedule THUNK to be run
+upon every update."
+  (%agenda-schedule *current-agenda*
+                    (lambda ()
+                      (thunk)
+                      (agenda-schedule-interval thunk interval interval))
+                    delay))
+
+(define (update-agenda)
+  "Update the current agenda."
+  (%update-agenda *current-agenda*))
+
+(define (clear-agenda)
+  "Clear the current agenda."
+  (%clear-agenda *current-agenda*))
+
+(define* (wait #:optional (delay 1))
+  "Yield coroutine and schdule the continuation to be run after DELAY
+ticks."
+  (yield (lambda (resume) (agenda-schedule resume delay))))
---
+++ src/emacsy/coroutine.scm
@@ -0,0 +1,157 @@
+;;; guile-2d
+;;; Copyright (C) 2013 David Thompson <dthompson2@worcester.edu>
+;;;
+;;; Guile-2d is free software: you can redistribute it and/or modify it
+;;; under the terms of the GNU Lesser General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; Guile-2d is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; Lesser General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with this program.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Cooperative multi-tasking.
+;;
+;;; Code:
+
+(define-module (emacsy coroutine)
+  #:export (coroutine
+            colambda
+            codefine
+            codefine*
+            coroutine->cid
+            strip-optargs
+            )
+  #:replace (yield))
+
+(define cid-next 0)
+
+(define (coroutine->cid resume)
+  "Given a coroutine resumption method. This will return that
+coroutine's ID, or cid."
+  (procedure-property resume 'cid))
+
+;; could have a (make-coroutine thunk) => (cid . run-coroutine-thunk)
+(define (make-coroutine thunk)
+  "Creates a procedure that can yield a continuation.  (Does not execute thunk.)"
+  (define cid cid-next)
+  (define (handler cont callback . args)
+    (define (resume . args)
+      (format #t "resuming cid ~a~%" cid)
+      ;; Call continuation that resumes the procedure.
+      (call-with-prompt 'coroutine-prompt
+                        (lambda () (apply cont args))
+                        handler))
+    (set-procedure-property! resume 'cid cid)
+    (when (procedure? callback)
+      (apply callback resume args)))
+  (set! cid-next (1+ cid-next))
+
+  ;; Call procedure.
+  (let ((first-call (lambda () (call-with-prompt 'coroutine-prompt thunk handler))))
+    (set-procedure-property! first-call 'cid cid)
+    (values first-call cid)))
+
+(define (coroutine thunk)
+  "Calls a procedure that can yield a continuation."
+  ((make-coroutine thunk)))
+
+;; emacs: (put 'colambda 'scheme-indent-function 0)
+(define-syntax-rule (colambda args body ...)
+  "Syntacic sugar for a lambda that is run as a coroutine."
+  (lambda args
+    (coroutine
+     (lambda () body ...))))
+
+;; emacs: (put 'codefine 'scheme-indent-function 1)
+(define-syntax-rule (codefine (name ...) . body)
+  "Syntactic sugar for defining a procedure that is run as a
+coroutine."
+  (define (name ...)
+    ;; Create an inner procedure with the same signature so that a
+    ;; recursive procedure call does not create a new prompt.
+    (define (name ...) . body)
+    (coroutine
+     (lambda () (name ...)))))
+
+;; emacs: (put 'codefine* 'scheme-indent-function 1)
+#;(define-syntax-rule (codefine* (name ...) . body)
+  "Syntactic sugar for defining a procedure with optional and
+keyword arguments that is run as a coroutine."
+  (define* (name ...)
+    ;; Create an inner procedure with the same signature so that a
+    ;; recursive procedure call does not create a new prompt.
+    (define* (name ...) . body)
+    (coroutine
+     (lambda () (name ...)))))
+
+(define-syntax strip-optargs
+  (lambda (x)
+    (syntax-case x ()
+      ((_)
+       #''())
+      ((_ (v e))
+       #'(cons v '()))
+      ((_ (v e) e2 ...)
+       #'(cons v (strip-optargs e2 ...)))
+      ;; ((_ (v e) e2)
+      ;;  #''(v (strip-optargs e2)))
+      ((_ e1)
+       (keyword? (syntax->datum #'e1))
+       #''())
+      ((_ e1 e2 ...)
+       (keyword? (syntax->datum #'e1))
+       #'(strip-optargs e2 ...))
+      ((_ e1)
+       #'(cons e1 '()))
+      ((_ e1 e2 ...)
+       #'(cons e1 (strip-optargs e2 ...)))
+      ;; ((_ e1 e2)
+      ;;  #`'(e1 #,@(strip-optargs e2)))
+      )))
+
+  "Syntactic sugar for defining a procedure with optional and
+keyword arguments that is run as a coroutine."
+(define-syntax codefine*
+  (lambda (x)
+    (syntax-case x ()
+      ((codefine* (name . args) . body) 
+       (with-syntax ((callable-args (datum->syntax x '()))) ;;#'(strip-optargs args)
+         #'(define* (name . args)
+             ;; Create an inner procedure with the same signature so that a
+             ;; recursive procedure call does not create a new prompt.
+             (define* (name . args) . body)
+             (coroutine
+              (lambda () (name .  callable-args)))))))))
+
+
+#;(define (strip-optargs args)
+ (define (strip-optargs* args)
+   (if (null? args)
+       '()
+       (let ((arg (syntax->datum (car args))))
+         (cond
+          ((keyword? arg)
+           ;; strip the keywords.
+           (strip-optargs (cdr args)))
+          ((pair? arg)
+           ;; only take the first of pairs.
+           (cons (caar args) (strip-optargs (cdr args))))
+          (else
+           ;; take everything else.
+           (cons (car args) (strip-optargs (cdr args))))))))
+ (let ((out (strip-optargs* args)))
+   (format #t "strip-optargs: ~a -> ~a~%" args out)
+   out))
+
+
+(define (yield callback)
+  "Yield continuation to a CALLBACK procedure."
+  (abort-to-prompt 'coroutine-prompt callback))
